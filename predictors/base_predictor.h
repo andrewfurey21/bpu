db92439bf3ce6e89ede7303134eaac8ede640677
Andrew Furey <andrewfurey2003@gmail.com>
2025-04-15 13:47:18 +0100

    move correction for ooo out of bpu logic

--- predictors/base_predictor.h
+++ predictors/base_predictor.h
@@ -1,6 +1,7 @@
 #include <bitset>
 #include <cstdint>
 #include <map>
+#include <utility>
 
 #ifndef BRANCH_PREDICTOR_H
 #define BRANCH_PREDICTOR_H
@@ -10,26 +11,40 @@
 
 // TODO: make on config file
 #define GHR_LENGTH 64
+typedef std::bitset<GHR_LENGTH> GHR_t;
 
 class BranchPredictor {
 public:
-  virtual bool predict(uint64_t pc, uint64_t seq_no, uint8_t piece) = 0;
-  virtual void update(uint64_t pc, uint64_t seq_no, uint8_t piece,
-                      bool actual) = 0;
+  bool predict(uint64_t pc, uint64_t seq_no, uint8_t piece) {
+    auto key = std::make_pair(seq_no, piece);
+    auto value = predict_branch(pc, ghr);
+    ghr_vector[key] = value;
+    return value.second >= 0;
+  }
+
+  void update(uint64_t pc, uint64_t seq_no, uint8_t piece, bool actual) {
+    auto key = std::make_pair(seq_no, piece);
+    int32_t prediction = ghr_vector[key].second;
+    std::bitset old_ghr = ghr_vector[key].first;
+    correct_state(pc, old_ghr, prediction, actual);
+    ghr_vector.erase(key);
+  }
+
   virtual uint64_t size() const = 0;
 
-  // this updates the ghr speculatively.
   void history_update(uint64_t pc, bool taken) {
     ghr <<= 1;
     ghr[0] = ghr[0] | taken;
   };
 
 protected:
-  std::map<std::pair<uint64_t, uint8_t>,
-           std::pair<std::bitset<GHR_LENGTH>, int32_t>>
-      ghr_vector;
+  virtual std::pair<GHR_t, int32_t> predict_branch(uint64_t pc, GHR_t ghr) = 0;
+  virtual void correct_state(uint64_t pc, GHR_t ghr, int32_t prediction,
+                             bool actual) = 0;
 
-  std::bitset<GHR_LENGTH> ghr;
+  std::map<std::pair<uint64_t, uint8_t>, std::pair<GHR_t, int32_t>> ghr_vector;
+
+  GHR_t ghr;
 };
 
 #endif
--- predictors/perceptron.h
+++ predictors/perceptron.h
@@ -1,14 +1,14 @@
 #include <algorithm>
 #include <array>
+#include <assert.h>
 #include <bitset>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
+#include <iostream>
 #include <map>
 #include <utility>
 #include <vector>
-#include <iostream>
-#include <assert.h>
 
 #include "base_predictor.h"
 
@@ -16,62 +16,59 @@
 #define PERCEPTRON_PREDICTOR_H
 
 #define INDEX_SIZE_BITS 10
-// TODO: size of each weight
+#define NUM_OUTPUT_BITS 16
+#define NUM_WEIGHT_BITS 8
+#define THRESHOLD ((int32_t)floor(1.93f * GHR_LENGTH + 14))
 
 #define NUM_OF_WEIGHTS (GHR_LENGTH + 1)
-#define THRESHOLD ((int32_t)floor(1.93f * GHR_LENGTH + 14))
 #define TABLE_SIZE (1 << INDEX_SIZE_BITS)
+#define WEIGHT_TYPE int64_t
+#define MAX_WEIGHT (WEIGHT_TYPE)((1 << NUM_WEIGHT_BITS) - 1) / 2
+#define MIN_WEIGHT (WEIGHT_TYPE)(-MAX_WEIGHT - 1)
+#define MAX_OUTPUT (WEIGHT_TYPE)((1 << NUM_OUTPUT_BITS) - 1) / 2
+#define MIN_OUTPUT (WEIGHT_TYPE)(-MAX_OUTPUT - 1)
 
 class Perceptron : public BranchPredictor {
-public:
-  Perceptron(void) : table({0}) {}
 
-  bool predict(uint64_t pc, uint64_t seq_no, uint8_t piece) override {
-    // std::cout << "Predict Seq no: " << seq_no << "\n";
-
+  std::pair<GHR_t, int32_t> predict_branch(uint64_t pc, GHR_t ghr) override {
     uint64_t index = hash(pc, ghr);
     int32_t prediction = 0;
-
-    prediction += table[index][0]; // bias
+    prediction += table[index][0];
     for (int i = 1; i < NUM_OF_WEIGHTS; i++) {
       if (ghr[i - 1])
-        prediction += table[index][i];
+        prediction =
+            std::clamp(prediction + table[index][i], MIN_OUTPUT, MAX_OUTPUT);
       else
-        prediction -= table[index][i];
+        prediction =
+            std::clamp(prediction - table[index][i], MIN_OUTPUT, MAX_OUTPUT);
     }
-
-    auto key = std::make_pair(seq_no, piece);
-    auto value = std::make_pair(ghr, prediction);
-    ghr_vector[key] = value;
-
-    return prediction >= 0;
+    return std::make_pair(ghr, prediction);
   }
 
-  void update(uint64_t pc, uint64_t seq_no, uint8_t piece, bool actual) override {
-    // std::cout << "Update Seq no: " << seq_no << "\n";
-    auto key = std::make_pair(seq_no, piece);
-    int32_t prediction = ghr_vector[key].second;
-    std::bitset old_ghr = ghr_vector[key].first;
+public:
+  Perceptron(void) : table({0}) {}
+  void correct_state(uint64_t pc, GHR_t ghr, int32_t prediction,
+                     bool actual) override {
 
     if ((prediction >= 0) != actual || abs(prediction) <= THRESHOLD) {
-      int index = hash(pc, old_ghr);
-      int32_t sign_actual = actual ? 1 : -1;
+      int index = hash(pc, ghr);
+      int64_t sign_actual = actual ? 1 : -1;
 
-      table[index][0] += sign_actual;
+      table[index][0] =
+          std::clamp(table[index][0] + sign_actual, MIN_WEIGHT, MAX_WEIGHT);
       for (int i = 1; i < NUM_OF_WEIGHTS; i++) {
-        int32_t sign_history = old_ghr[i - 1] ? 1 : -1;
-        table[index][i] += sign_actual * sign_history;
+        int32_t sign_history = ghr[i - 1] ? 1 : -1;
+        table[index][i] =
+            std::clamp(table[index][i] + sign_actual * sign_history, MIN_WEIGHT,
+                       MAX_WEIGHT);
       }
     }
-    ghr_vector.erase(key);
   };
 
-  uint64_t size() const override {
-    return 0;
-  }
+  uint64_t size() const override { return 0; }
 
 private:
-  uint64_t folded_history(std::bitset<GHR_LENGTH> ghr) const {
+  uint64_t folded_history(GHR_t ghr) const {
     uint64_t fghr = 0;
     for (int i = 0; i < GHR_LENGTH; i += INDEX_SIZE_BITS) {
       fghr ^= (TABLE_SIZE - 1) & (ghr >> i).to_ulong();
@@ -79,11 +76,11 @@ private:
     return fghr;
   }
 
-  uint64_t hash(uint64_t pc, std::bitset<GHR_LENGTH> current_ghr) const {
-    return (pc % TABLE_SIZE) ^ folded_history(current_ghr);
+  uint64_t hash(uint64_t pc, GHR_t ghr) const {
+    return (pc % TABLE_SIZE) ^ folded_history(ghr);
   }
 
-  std::array<std::array<int32_t, NUM_OF_WEIGHTS>, TABLE_SIZE> table;
+  std::array<std::array<WEIGHT_TYPE, NUM_OF_WEIGHTS>, TABLE_SIZE> table;
 };
 
 #endif
